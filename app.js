6112f6a0db58e2586d6279c7734f070b68dce047
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-02-17 10:52:01 +0200

    added management

--- app.js
+++ app.js
@@ -17,6 +17,7 @@
 
     // KÕIK MUUTUJAD, mis on üldised ja muudetavad
     this.currentRoute = null; // hoian meeles mis lehel olen (home-view, ...)
+    this.interval = null;
 
 
 
@@ -31,20 +32,30 @@
         // käivitan siis kui jõuan lehele
         console.log('JS avalehel');
 
+        // kui olemas, teen nulliks
+        if(this.interval){ clearInterval(this.interval); }
+
         // kui jõuan avalehele siis käivitub timer, mis hakkab trükkima kulunud sekundeid
         // divi sisse #counter
         // hakkab 0st
+        var seconds = 0;
+        this.interval = window.setInterval(function(){
+          seconds++;
+          document.querySelector('#counter').innerHTML = seconds;
+        }, 1000); //iga 1000ms tagant käivitub
 
       }
     },
     "list-view": {
       render: function(){
         console.log('JS loendi lehel');
+
       }
     },
     "manage-view": {
       render: function(){
         console.log('JS halduse lehel');
+
       }
     }
   };
@@ -75,27 +86,88 @@
       document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
     },
     addNewClick: function(event){
-      //console.log(event);
-      this.click_count++;
-      console.log(this.click_count);
+      // lisa uus purk
+      var title = document.querySelector('.title').value;
+      var ingredients = document.querySelector('.ingredients').value;
+      console.log(title + ' ' + ingredients);
+
+      var new_jar = new Jar(title, ingredients);
+      var li = new_jar.createHtmlElement();
+      document.querySelector('.list-of-jars').appendChild(li);
+
 
     },
     routeChange: function(event){
 
+      // slice võtab võtab # ära #home-view >> home-view
       this.currentRoute = window.location.hash.slice(1);
 
       // kas leht on olemas
       if(this.routes[this.currentRoute]){
         //jah
+
+        this.updateMenu();
+
         console.log('>>> ' + this.currentRoute);
         //käivitan selle lehe jaoks ettenähtud js
         this.routes[this.currentRoute].render();
       }else{
         // 404?
         console.log('404');
-        window.location.hash = 'home-views';
+        window.location.hash = 'home-view';
       }
 
+    },
+
+    updateMenu: function(){
+
+      //kui on mingil menüül klass active-menu siis võtame ära
+      document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace(' active-menu', '');
+
+      //käesolevale lehele lisan juurde
+      document.querySelector('.' + this.currentRoute).className += ' active-menu';
+
+    }
+
+  };
+
+
+  var Jar = function(new_title, new_ingredients){
+    this.title = new_title;
+    this.ingredients = new_ingredients;
+  };
+
+  Jar.prototype = {
+    createHtmlElement: function(){
+      // anda tagasi ilus html
+
+      // li
+      //   span.letter
+      //     M
+      //   span.content
+      //     Maasikamoos | maasikas, õun
+
+      var li = document.createElement('li');
+
+      var span = document.createElement('span');
+      span.className = 'letter';
+
+      var letter = document.createTextNode(this.title.charAt(0));
+      span.appendChild(letter);
+
+      li.appendChild(span);
+
+      var content_span = document.createElement('span');
+      content_span.className = 'content';
+
+      var content = document.createTextNode(this.title + ' | ' + this.ingredients);
+      content_span.appendChild(content);
+
+      li.appendChild(content_span);
+
+      console.log(li);
+
+      return li;
     }
   };
 
